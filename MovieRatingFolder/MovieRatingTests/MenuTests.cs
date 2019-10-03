ea9cadc2baf9a970ae5f263be21953081efeb7b2
keddetheb <kedde_b@live.dk>
2019-10-03 10:13:41 +0200

    poj

---
+++ MovieRatingFolder/MovieRatingTests/MenuTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieRating;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRating.Tests
+{
+    [TestClass()]
+    public class MenuTests
+    {
+        Menu menu = new Menu();
+        [TestMethod()]
+        public void FindReviewsByReviewerTest()
+        {
+            int name = 563;
+
+            Assert.AreEqual(menu.FindReviewsByReviewer(name), 14);
+        }
+    }
+}
